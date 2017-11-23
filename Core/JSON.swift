cda0a095b4c65c42d93ec805f9979a62d536ec85
Carl Chen <ccf.developer@gmail.com>
2017-11-23 12:53:30 +0800

    Change API to avoid name conflict with Swift Foundation framework.

--- Core/JSON.swift
+++ Core/JSON.swift
@@ -8,31 +8,103 @@
 
 import Foundation
 
-public struct JSON {
-    public enum Errors: Error {
-        case toDictionaryFailed(Any)
-        case toArrayFailed(Any)
-        case toTypedArrayFailed(Any)
+public struct Transformer {
+    public struct JSON {
+        public enum Errors: Error {
+            case toDictionaryFailed(Any)
+            case toArrayFailed(Any)
+            case toTypedArrayFailed(Any)
+        }
+
+        public static let toDictionary: (Any) throws -> [String: Any] = {
+            (obj: Any) -> [String: Any] in
+            guard let dic = obj as? [String: Any] else {
+                throw Errors.toDictionaryFailed(obj)
+            }
+            return dic
+        }
+
+        public static let toArray: (Any) throws -> [Any] = { (obj: Any) -> [Any] in
+            guard let array = obj as? [Any] else {
+                throw Errors.toArrayFailed(obj)
+            }
+            return array
+        }
+
+        public static func toTypedArray<T>(_ obj: Any) throws -> [T] {
+            guard let result = obj as? [T] else {
+                throw Errors.toTypedArrayFailed(obj)
+            }
+            return result
+        }
     }
 
-    public static let toDictionary: (Any) throws -> [String: Any] = { (obj: Any) -> [String: Any] in
-        guard let dic = obj as? [String: Any] else {
-            throw Errors.toDictionaryFailed(obj)
+
+    public struct Dictionary {
+        public enum Errors: Error {
+            case noValueFromKey(String)
+            case valueTypeWrong(String, Any)
+            case notJson(Any)
+            case toModelFailed(String)
         }
-        return dic
+
+        public static func value<T>(key: String) -> ([String: Any]) throws -> T {
+            return { (dic: [String: Any]) -> T in
+                guard let value = dic[key] else {
+                    throw Errors.noValueFromKey(key)
+                }
+
+                guard let result = value as? T else {
+                    throw Errors.valueTypeWrong(String(describing: T.self), value)
+                }
+                return result
+            }
+        }
+
+        public func toModel<T>(_ dic: [String: Any]) throws -> T where T: Codable {
+            let jsonData: Data
+            do {
+                jsonData = try JSONSerialization.data(withJSONObject: dic
+                    , options: JSONSerialization.WritingOptions())
+            } catch {
+                throw Errors.notJson(dic)
+            }
+
+            do {
+                return try JSONDecoder().decode(T.self, from: jsonData)
+            } catch {
+                throw Errors.toModelFailed(String(describing: T.self))
+            }
+        }
     }
 
-    public static let toArray: (Any) throws -> [Any] = { (obj: Any) -> [Any] in
-        guard let array = obj as? [Any] else {
-            throw Errors.toArrayFailed(obj)
+
+    public struct Array {
+        public enum Errors: Error {
+            case notJson(Any)
+            case toModelArrayFailed(String)
         }
-        return array
+
+        public func toModelArray<T>(_ dicArray: [[String: Any]]
+            , throwWhenError: Bool = false) throws -> [T] where T: Codable {
+
+            let result: [T] = try dicArray.flatMap { (dic) -> T? in
+                do {
+                    let dicData = try JSONSerialization.data(withJSONObject: dic
+                        , options: JSONSerialization.WritingOptions())
+                    return try JSONDecoder().decode(T.self, from: dicData)
+                } catch {
+                    if throwWhenError {
+                        throw Errors.toModelArrayFailed(String(describing: T.self))
+                    } else {
+                        return nil
+                    }
+                }
+            }
+
+            return result
+        }
     }
 
-    public static func toTypedArray<T>(_ obj: Any) throws -> [T] {
-        guard let result = obj as? [T] else {
-            throw Errors.toTypedArrayFailed(obj)
-        }
-        return result
-    }
 }
+
---
+++ README.md
@@ -0,0 +1,15 @@
+[![Carthage compatible](https://img.shields.io/badge/Carthage-compatible-4BC51D.svg?style=flat)](https://github.com/Carthage/Carthage)
+[![Platform](https://img.shields.io/badge/Platform-iOS|macOS|tvOS-4BC51D.svg?style=flat)](https://github.com/webfrogs/HandOfTheKing)
+
+Transformers is a framework to transform things elegantly using the power of Swift programming language. 
+
+Carthage support only. 
+
+## Feature
+
+- Type casting of JSON object.
+- Cast a swift dictionary to swift model which confirms `Codable` protocol.
+- Cast a swift array whose item type is dictionary to swift model array whose item confirms  `Codable` protocol.
+
+## Usage
+
