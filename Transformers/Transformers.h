286343addcc2f0f8ee4f8710df32a1d3600b4187
Carl Chen <ccf.developer@gmail.com>
2017-11-23 10:05:16 +0800

    init

---
+++ Transformers/Transformers.h
@@ -0,0 +1,19 @@
+//
+//  Transformers.h
+//  Transformers
+//
+//  Created by Carl Chen on 23/11/2017.
+//  Copyright © 2017 nswebfrog. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Transformers.
+FOUNDATION_EXPORT double TransformersVersionNumber;
+
+//! Project version string for Transformers.
+FOUNDATION_EXPORT const unsigned char TransformersVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Transformers/PublicHeader.h>
+
+
