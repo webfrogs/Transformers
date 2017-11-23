422dc06068a3ed7553c145a7d496914a1f5d51ca
Carl Chen <ccf.developer@gmail.com>
2017-11-23 12:01:22 +0800

    Add support to macOS and tvOS

---
+++ Transformers-macOS/Transformers_macOS.h
@@ -0,0 +1,19 @@
+//
+//  Transformers_macOS.h
+//  Transformers-macOS
+//
+//  Created by Carl Chen on 23/11/2017.
+//  Copyright © 2017 nswebfrog. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Transformers_macOS.
+FOUNDATION_EXPORT double Transformers_macOSVersionNumber;
+
+//! Project version string for Transformers_macOS.
+FOUNDATION_EXPORT const unsigned char Transformers_macOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Transformers_macOS/PublicHeader.h>
+
+
---
+++ Transformers-tvOS/Transformers_tvOS.h
@@ -0,0 +1,19 @@
+//
+//  Transformers_tvOS.h
+//  Transformers-tvOS
+//
+//  Created by Carl Chen on 23/11/2017.
+//  Copyright © 2017 nswebfrog. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Transformers_tvOS.
+FOUNDATION_EXPORT double Transformers_tvOSVersionNumber;
+
+//! Project version string for Transformers_tvOS.
+FOUNDATION_EXPORT const unsigned char Transformers_tvOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Transformers_tvOS/PublicHeader.h>
+
+
